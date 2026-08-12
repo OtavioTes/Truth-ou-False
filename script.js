c09c382d4bf01da12e61f6840b10cb468bc7df14
Otavio T. <otaviotesolim123@gmail.com>
2026-08-12 17:28:33 -0300

    Script correto pg1

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 function mudarIdioma(idioma) {
 
     if (idioma === "pt") {
+
         document.getElementById("titulo").textContent =
             "Teste de intensificador de fake news";
 
         document.getElementById("introducao").innerHTML =
             'Olá investigador(a), seu trabalho de hoje é ler notícias e decidir se elas são <i class="verde">VERDADEIRAS</i> ou <i class="vermelho">FALSAS</i>.';
 
-        document.getElementById("verdadeiro").textContent =
-            "VERDADEIRO";
+        document.getElementById("informacao").textContent =
+            "Escolhida a língua, vamos para o teste. Teremos 5 testes em cada idioma, ou seja, terminou um pode ir pro outro :]";
 
-        document.getElementById("falso").textContent =
-            "FALSO";
+        document.getElementById("instrucao").textContent =
+            "Clique no botão abaixo fazendo o favor.";
+
+        document.getElementById("botaoTeste").textContent =
+            "COMEÇAR TESTE";
+
+        document.getElementById("botaoTeste").href =
+            "teste-pt1.html";
     }
 
     if (idioma === "en") {
+
         document.getElementById("titulo").textContent =
             "Fake News Detection Test";
 
         document.getElementById("introducao").innerHTML =
             'Hello investigator! Your job is to read the news and decide whether they are <i class="verde">TRUE</i> or <i class="vermelho">FALSE</i>.';
 
-        document.getElementById("verdadeiro").textContent =
-            "TRUE";
+        document.getElementById("informacao").textContent =
+            "Language chosen, let's go to the test. There will be 5 tests in each language, so after finishing one, you can move on to the next one :]";
 
-        document.getElementById("falso").textContent =
-            "FALSE";
+        document.getElementById("instrucao").textContent =
+            "Please click the button below.";
+
+        document.getElementById("botaoTeste").textContent =
+            "START TEST";
+
+        document.getElementById("botaoTeste").href =
+            "teste-en1.html";
     }
 }
